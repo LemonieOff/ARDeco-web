0399dd4cf2978b0638ea7629f036fc3af91733c0
valentin-epitech <valentin.astic@epitech.eu>
2023-05-13 19:47:15 +0200

    [+] Reworking the team page, adding new style and past pro experiences [+]

---
+++ components/Profiles/ProfileLouis.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="profile-page-content" id="louis-profile">
+        <div class="sub-content">
+            <div class="personal-content">
+                <a href="https://www.linkedin.com/in/louis-villedieu-8866091b9/">
+                    <img class="profile-picture" src="../../assets/images/profile-pictures/LOUIS.png">
+                </a>
+                <div class="black-separation-bar"></div>
+                <div class="orange-separation-bar"></div>
+                <div class="name">Louis Villedieu</div>
+                <div class="role">AR Developer</div>
+            </div>
+            <div class="timeline-content">
+                <div class="year-2020">2020</div>
+                <div class="timeline-inactive">
+                    <div class="year-bubble-inactive"></div>
+                </div>
+                <div class="timeline-inactive"></div>
+                <div class="timeline-active">
+                    <div class="year-bubble-active"></div>
+                    <img class="internship-logo" src="../../assets/images/compagny-logos/BCAExpertiseLogo.png">
+                    <div class="internship-role">Junior Software Developer</div>
+                </div>
+                <div class="timeline-inactive">
+                    <a href="https://www.epitech.eu/en/">
+                        <img class="epitech-logo-timeline" src="../../assets/images/profile-pictures/Epitech.png">
+                    </a>
+                </div>
+                <div class="timeline-active">
+                    <div class="year-bubble-active"></div>
+                    <img class="internship-logo" src="../../assets/images/compagny-logos/VersityLogo.png">
+                    <div class="internship-role">Web Developer</div>
+                </div>
+                <div class="timeline-active">
+                    <div class="end-year-bubble-active"></div>
+                    <img class="internship-logo" src="../../assets/images/compagny-logos/WorkingGameLogo.png">
+                    <div class="internship-role">Video Game Developer</div>
+                </div>
+                <div class="year-2023">2023</div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "ProfileLouis"
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/TeamPage.scss';
+</style>
---
+++ components/Profiles/ProfileMathias.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="profile-page-content" id="mathias-profile">
+        <div class="sub-content">
+            <div class="personal-content">
+                <a href="https://www.linkedin.com/in/mathias-blanchard-a54818182/">
+                    <img class="profile-picture" src="../../assets/images/profile-pictures/MATHIAS.png">
+                </a>
+                <div class="black-separation-bar"></div>
+                <div class="orange-separation-bar"></div>
+                <div class="name">Mathias Blanchard</div>
+                <div class="role">Mobile Developer</div>
+            </div>
+            <div class="timeline-content">
+                <div class="year-2020">2020</div>
+                <div class="timeline-inactive">
+                    <div class="year-bubble-inactive"></div>
+                </div>
+                <div class="timeline-inactive"></div>
+                <div class="timeline-active">
+                    <div class="year-bubble-active"></div>
+                    <img class="internship-logo" src="../../assets/images/compagny-logos/CognitiveEnginesLogo.png">
+                    <div class="internship-role">Image processing and depth camera vision</div>
+                </div>
+                <div class="timeline-inactive">
+                    <a href="https://www.epitech.eu/en/">
+                        <img class="epitech-logo-timeline" src="../../assets/images/profile-pictures/Epitech.png">
+                    </a>
+                </div>
+                <div class="timeline-inactive">
+                    <div class="year-bubble-inactive"></div>
+                </div>
+                <div class="timeline-inactive">
+                    <div class="end-year-bubble-inactive"></div>
+                </div>
+                <div class="year-2023">2023</div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: "ProfileMathias"
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../styles/TeamPage.scss';
+</style>
