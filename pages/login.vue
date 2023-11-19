b4fe22764d0d25f528c49049bc387ea854eda5ae
IndiGan <72012215+IndiGan@users.noreply.github.com>
2023-11-19 17:49:56 +0100

    Register form

--- pages/login.vue
+++ pages/login.vue
@@ -135,21 +135,56 @@ const logout = async () => {
             Chargement en cours...
         </div>
         <div id="forms-wrapper" style="display: none">
-            <div id="login-form" class="form">
-                <div id="email_section">
-                    <label for="email">Email : </label><input type="text" id="email" name="email" placeholder="Email">
-                    <ul id="email_errors" class="login-error"></ul>
+            <div id="login-register-wrapper" class="login-register-wrapper">
+                <div id="login-form" class="form">
+                    <div id="email_section_login">
+                        <label for="email">Email : </label><input type="text" id="email_login" name="email" placeholder="Email">
+                        <ul id="email_errors_login" class="login-error"></ul>
+                    </div>
+                    <div id="password_section_login">
+                        <label for="password">Password : </label><input type="password" id="password_login" name="password"
+                                                                        placeholder="Password">
+                        <ul id="password_errors_login" class="login-error"></ul>
+                    </div>
+                    <div id="general_section_login">
+                        <ul id="general_errors_login" class="login-error"></ul>
+                        <ul id="general_success_login" class="login-success"></ul>
+                    </div>
+                    <button @click="login">Login</button>
                 </div>
-                <div id="password_section">
-                    <label for="password">Password : </label><input type="password" id="password" name="password"
-                                                                    placeholder="Password">
-                    <ul id="password_errors" class="login-error"></ul>
+                <div id="register-form" class="form">
+                    <div id="email_section_register">
+                        <label for="email">Email : </label><input type="text" id="email_register" name="email" placeholder="Email">
+                        <ul id="email_errors_register" class="login-error"></ul>
+                    </div>
+                    <div id="password_section_register">
+                        <label for="password">Password : </label><input type="password" id="password_register" name="password"
+                                                                        placeholder="Password">
+                        <ul id="password_errors_register" class="login-error"></ul>
+                    </div>
+                    <div id="password_confirm_section_register">
+                        <label for="password_confirm_register">Password confirm : </label><input type="password" id="password_confirm_register" name="password_confirm"
+                                                                        placeholder="Password">
+                        <ul id="password_confirm_errors_register" class="login-error"></ul>
+                    </div>
+                    <div id="first_name_section_register">
+                        <label for="first_name_register">First name : </label><input type="text" id="first_name_register" name="first_name" placeholder="Hugo">
+                        <ul id="first_name_errors_register" class="login-error"></ul>
+                    </div>
+                    <div id="last_name_section_register">
+                        <label for="last_name_register">Last name : </label><input type="text" id="last_name_register" name="last_name" placeholder="BECART">
+                        <ul id="last_name_errors_register" class="login-error"></ul>
+                    </div>
+                    <div id="city_section_register">
+                        <label for="city_register">City : </label><input type="text" id="city_register" name="city" placeholder="Berlin">
+                        <ul id="city_errors_register" class="login-error"></ul>
+                    </div>
+                    <div id="general_section_register">
+                        <ul id="general_errors_register" class="login-error"></ul>
+                        <ul id="general_success_register" class="login-success"></ul>
+                    </div>
+                    <button @click="register">Register</button>
                 </div>
-                <div id="general_section">
-                    <ul id="general_errors" class="login-error"></ul>
-                    <ul id="general_success" class="login-success"></ul>
-                </div>
-                <button @click="login">Login</button>
             </div>
             <div id="user-form" class="form">
                 <div id="user-welcome"></div>
@@ -167,6 +202,17 @@ const logout = async () => {
     align-items: center;
     justify-content: center;
     margin: 10% 0;
+}
+
+.login-register-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    .form {
+        padding: 0 25px;
+    }
 }
 
 .login-error {
