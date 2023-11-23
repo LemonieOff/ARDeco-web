bcfdd2b26ae03f4a80cbcc10a24a042a542e94ac
valentin-epitech <valentin.astic@epitech.eu>
2023-11-23 19:34:45 +0100

    Removing tests on the online env playwright

--- tests/mainPage.spec.ts
+++ tests/mainPage.spec.ts
@@ -1,26 +1,6 @@
 import { test, expect, type Page } from '@playwright/test';
 
 test.describe('Main Page', () => {
-  test('[Prod] Main page with computer resolution', async ({ page }) => {
-    await page.goto('https://ardeco.app/');
-    page.setViewportSize({width: 1920, height: 966});
-    const navbar = await page.locator('.navbar');
-    await expect(navbar).toHaveScreenshot();
-
-    const frontPage = await page.locator('#hero');
-    await expect(frontPage).toHaveScreenshot();
-  });
-
-  test('[Prod] Main page with mobile resolution', async ({ page }) => {
-    await page.goto('https://ardeco.app/');
-    page.setViewportSize({width: 393, height: 851});
-    const navbar = await page.locator('.navbar');
-    await expect(navbar).toHaveScreenshot();
-
-    const frontPage = await page.locator('#hero');
-    await expect(frontPage).toHaveScreenshot();
-  });
-
   test('[Localhost] Main page with computer resolution', async ({ page }) => {
     await page.goto('http://localhost:3000/');
     page.setViewportSize({width: 1920, height: 966});
