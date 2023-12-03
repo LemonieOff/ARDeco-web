5fdab114fa33535050cd16995c7b925ff6e1ae68
valentin-epitech <valentin.astic@epitech.eu>
2023-12-03 17:14:13 +0100

    Adding getGallery button

--- components/UserProfile/ProfileDetails.vue
+++ components/UserProfile/ProfileDetails.vue
@@ -2,7 +2,12 @@
     <div class="profile-details">
         <img class="profile-picture" src="../../assets/images/profile-settings/default-profile-picture.png">
         <div class="first-and-last-names">First and last name</div>
-        <button @click="getSettings">Get user settings</button>
+        <br>
+        <br>
+        <hr>
+        <button class="buttonSettings" @click="getSettings">Get user settings</button>
+        <button class="buttonSettings" @click="getGallery">Get user gallery</button>
+        <div id="reponseText" class="buttonSettingsResponse"></div>
     </div>
 </template>
 
@@ -26,6 +31,26 @@ export default {
 
             const result = await response.json();
             console.log(result);
+            document.getElementById('reponseText').innerHTML = result.description;
+        },
+        async getGallery() {
+            const userID = localStorage.getItem('userID');
+            if (userID == null) {
+                console.log('No user found, redirecting to login');
+                window.location.href = 'http://localhost:3000/login';
+                return;
+            };
+            const response = await fetch('https://api.ardeco.app/gallery/user/' + `${userID}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+            });
+
+            const result = await response.json();
+            console.log(result);
+            document.getElementById('reponseText').innerHTML = result.description;
         }
     }
 }
@@ -33,4 +58,17 @@ export default {
 
 <style lang="scss" scoped>
 @import "~/styles/ProfileSettings.scss";
+
+.buttonSettings {
+    width: 40%;
+    margin-left: 30%;
+    margin-top: 5%;
+}
+
+.buttonSettingsResponse {
+    text-align: center;
+    width: 80%;
+    margin-left: 10%;
+    margin-top: 5%;
+}
 </style>
