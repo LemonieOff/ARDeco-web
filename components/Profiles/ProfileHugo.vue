164f344ce18dd917c561b0c3623085fe16ffa5eb
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-03-21 02:27:24 +0100

    New translation system on team components

--- components/Profiles/ProfileHugo.vue
+++ components/Profiles/ProfileHugo.vue
@@ -9,7 +9,7 @@
                 <div class="black-separation-bar"></div>
                 <div class="orange-separation-bar"></div>
                 <div class="name">Hugo Becart</div>
-                <div id="role-hugo" class="role"></div>
+                <div id="role-hugo" class="role">{{ content.role }}</div>
             </div>
             <div class="timeline-content">
                 <div class="year-2020">2020</div>
@@ -20,7 +20,7 @@
                 <div class="timeline-active">
                     <div class="year-bubble-active"></div>
                     <img class="internship-logo" src="../../assets/images/compagny-logos/MyFlyingBoxLogo.png">
-                    <div id="internshipRole1-hugo" class="internship-role"></div>
+                    <div id="internshipRole1-hugo" class="internship-role">{{ content.internshipRole1 }}</div>
                 </div>
                 <div class="timeline-inactive">
                     <a href="https://www.epitech.eu/en/">
@@ -33,7 +33,7 @@
                 <div class="timeline-active">
                     <div class="end-year-bubble-active"></div>
                     <img class="internship-logo" src="../../assets/images/compagny-logos/WorkingGameLogo.png">
-                    <div id="internshipRole2-hugo" class="internship-role"></div>
+                    <div id="internshipRole2-hugo" class="internship-role">{{ content.internshipRole2 }}</div>
                 </div>
                 <div class="year-2023">2023</div>
             </div>
@@ -48,30 +48,37 @@ import fr from "~/src/lang/fr.json";
 export default {
     name: "ProfileHugo",
     props: {
-        urlLang: String
+        urlLang: String | null
+    },
+    data() {
+        return {
+            content: {},
+            langPrefix: "/"
+        }
     },
     mounted() {
-        let lang = this.urlLang
-        if (lang == null) {
-            lang = localStorage.getItem('lang')
+        let lang = this.urlLang;
+
+        // If lang selector is not passed in url, get the user's one or set it to french
+        if (lang !== 'en' && lang !== 'fr') {
+            const localStorageLang = localStorage.getItem('lang');
+            if (localStorageLang) {
+                lang = localStorageLang;
+            } else {
+                lang = 'fr';
+            }
         }
 
-        if (location.href.includes("/fr/") && localStorage.getItem('lang') == "en") {
-            location.href = location.href.replace("/fr/", "/en/")
-        } else if (location.href.includes("/en/") && localStorage.getItem('lang') == "fr") {
-            location.href = location.href.replace("/en/", "/fr/")
-        }
+        // Set the content variable to the correct language
+        this.content = lang === 'en' ? en.profilePages.hugo : fr.profilePages.hugo;
 
-        if (lang == 'en') {
-            document.getElementById('role-hugo').innerText = en.profilePages.hugo.role
-            document.getElementById('internshipRole1-hugo').innerText = en.profilePages.hugo.internshipRole1
-            document.getElementById('internshipRole2-hugo').innerText = en.profilePages.hugo.internshipRole2
-        } else {
-            document.getElementById('role-hugo').innerText = fr.profilePages.hugo.role
-            document.getElementById('internshipRole1-hugo').innerText = fr.profilePages.hugo.internshipRole1
-            document.getElementById('internshipRole2-hugo').innerText = fr.profilePages.hugo.internshipRole2
+        // Prefix for links
+        if (location.href.includes("/fr/")) {
+            this.langPrefix = "/fr/";
+        } else if (location.href.includes("/en/")) {
+            this.langPrefix = "/en/";
         }
-    },
+    }
 }
 </script>
 
