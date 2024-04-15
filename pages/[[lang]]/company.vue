43cdbd67d07aab6bf334be1546f7ae1f0fc5e9ce
Valentin <valentin.astic@epitech.eu>
2024-04-15 11:30:01 -0400

    [~] Files modifications to match existing folder arrangement style [~]

--- pages/[[lang]]/company.vue
+++ pages/[[lang]]/company.vue
@@ -1,11 +1,16 @@
 <template>
-    <Navbar/>
-    <CompanySettings/>
+    <Navbar :urlLang=lang></Navbar>
+    <CompanySettings :urlLang=lang></CompanySettings>
 </template>
 
 <script setup lang="ts">
 import Navbar from "~/components/Navbar.vue";
 import CompanySettings from "~/components/Settings/CompanySettings.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const lang = route.params.lang;
 </script>
 
 <style lang="scss" scoped></style>
--- pages/[[lang]]/settings.vue
+++ pages/[[lang]]/settings.vue
@@ -1,11 +1,16 @@
 <template>
-    <Navbar urlLang="fr"/>
-    <OverallSettings urlLang="fr"/>
+    <Navbar :urlLang=lang></Navbar>
+    <OverallSettings :urlLang=lang></OverallSettings>
 </template>
 
 <script setup lang="ts">
 import Navbar from "~/components/Navbar.vue";
 import OverallSettings from "~/components/Settings/OverallSettings.vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const lang = route.params.lang;
 </script>
 
 <style lang="scss" scoped></style>
