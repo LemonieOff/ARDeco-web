ae8f3f6f135a6b11762126b64be36f9a2ecb38d6
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-08-13 03:26:55 +0200

    Add support of Nuxt Tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 import {defineNuxtConfig} from "nuxt/config";
 
 export default defineNuxtConfig({
+    modules: ['@nuxtjs/tailwindcss'],
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
