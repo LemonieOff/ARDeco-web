ba539fe9ece9ef408b8384708fffa78addc46b4a
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-08-22 00:13:29 +0200

    Footer was using wrong translations, so it disappeared

--- components/Footer.vue
+++ components/Footer.vue
@@ -25,7 +25,7 @@ export default {
     name: "Footer",
     data() {
         return {
-            content: this.$content.navBar,
+            content: this.$content.footer,
             langPrefix: this.$langPrefix
         }
     }
