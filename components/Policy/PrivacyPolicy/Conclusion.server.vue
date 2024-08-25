3dd57c1654f5b28a6f27285024243c5350375fef
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-08-25 03:18:12 +0200

    Optimized privacy policy page

---
+++ components/Policy/PrivacyPolicy/Conclusion.server.vue
@@ -0,0 +1,22 @@
+<template>
+    <PolicyCard :title="content.title">
+        <div>{{ content.usage }}</div>
+        <div>{{ content.share }}</div>
+        <div>{{ content.userRights }}</div>
+        <div>{{ content.keep }}</div>
+        <div>{{ content.acceptance }}</div>
+        <div>{{ content.questions }}</div>
+        <div>{{ content.update }}</div>
+    </PolicyCard>
+</template>
+
+<script setup lang="ts">
+import PolicyCard from "@/components/Policy/PolicyCard.server.vue"
+
+defineProps({
+    content: {
+        type: Object,
+        required: true
+    }
+});
+</script>
