97542c8a9494c93d2a67c25ac91b696627d44373
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-08-25 03:06:52 +0200

    Optimized product page

--- pages/[[lang]]/product.vue
+++ pages/[[lang]]/product.vue
@@ -1,35 +1,25 @@
 <template>
-    <Html :lang="lang"></Html>
-    <Navbar/>
     <QuestionsList/>
-    <WhatIsArdeco/>
-    <div class="space-between-questions"></div>
-    <HowToUseArdeco/>
-    <div class="space-between-questions"></div>
-    <HowAreMyFurnitureChosen/>
-    <div class="space-between-questions"></div>
-    <WhereDoModelsComeFrom/>
-    <div class="space-between-questions"></div>
-    <WhyShouldIUseArdeco/>
-    <div class="space-between-questions"></div>
-    <InAppPurchaces/>
-    <div class="space-between-questions"></div>
-    <Footer/>
+    <WhatIsArdeco :content="content"/>
+    <HowToUseArdeco :content="content"/>
+    <HowAreMyFurnitureChosen :content="content"/>
+    <WhereDoModelsComeFrom :content="content"/>
+    <WhyShouldIUseArdeco :content="content"/>
+    <InAppPurchaces :content="content"/>
 </template>
 
 <script setup lang="ts">
-import Navbar from "~/components/Navbar.vue";
-import QuestionsList from "~/components/FAQ/QuestionsList.vue";
-import WhatIsArdeco from "~/components/FAQ/WhatIsArdeco.vue"
-import HowAreMyFurnitureChosen from "~/components/FAQ/HowAreMyFurnitureChosen.vue"
-import WhereDoModelsComeFrom from "~/components/FAQ/WhereDoModelsComeFrom.vue"
-import WhyShouldIUseArdeco from "~/components/FAQ/WhyShouldIUseArdeco.vue"
-import InAppPurchaces from "~/components/FAQ/InAppPurchaces.vue"
-import HowToUseArdeco from "~/components/FAQ/HowToUseArdeco.vue"
+import QuestionsList from "@/components/FAQ/QuestionsList.vue";
+import WhatIsArdeco from "@/components/FAQ/WhatIsArdeco.server.vue"
+import HowAreMyFurnitureChosen from "@/components/FAQ/HowAreMyFurnitureChosen.server.vue"
+import WhereDoModelsComeFrom from "@/components/FAQ/WhereDoModelsComeFrom.server.vue"
+import WhyShouldIUseArdeco from "@/components/FAQ/WhyShouldIUseArdeco.server.vue"
+import InAppPurchaces from "@/components/FAQ/InAppPurchaces.server.vue"
+import HowToUseArdeco from "@/components/FAQ/HowToUseArdeco.server.vue"
 
 const nuxtApp = useNuxtApp();
 
-const lang = ref(nuxtApp.$lang);
+const content = ref(nuxtApp.$content.productPages);
 </script>
 
 <style lang="scss" scoped>
