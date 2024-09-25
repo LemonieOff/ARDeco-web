adf71ca1e060725da255c0004f77153497bf3d17
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-09-25 05:00:15 +0200

    Remove a lot of useless code (after refactor) in login.vue

--- pages/[[lang]]/login.vue
+++ pages/[[lang]]/login.vue
@@ -6,7 +6,7 @@
         </div>
         <div v-else-if="userID" id="user-form">
             <div id="user-welcome">{{ content.welcome }}<span>{{ userID }}</span>!</div>
-            <button id="logout" @click="logout">{{ content.logout }}</button>
+            <button id="logout" @click="logoutUser">{{ content.logout }}</button>
         </div>
         <div v-else class="flex items-center justify-center">
             <div
@@ -117,7 +117,7 @@
 </template>
 
 <script setup lang="ts">
-import { disconnect, isLogged } from "public/ts/checkLogin";
+import { isLogged, logout } from "public/ts/checkLogin";
 import type { ShallowRef } from "vue";
 
 const nuxtApp = useNuxtApp();
@@ -144,77 +144,6 @@ onMounted(async () => {
     userID.value = await isLogged();
     loading.value = false;
 });
-
-const displayHTMLErrors = (result, response, type) => {
-    const errors = {
-        "email": document.getElementById(`email_errors_${type}`),
-        "password": document.getElementById(`password_errors_${type}`),
-        "password_confirm": document.getElementById(`password_confirm_errors_${type}`),
-        "first_name": document.getElementById(`first_name_errors_${type}`),
-        "last_name": document.getElementById(`last_name_errors_${type}`),
-        "city": document.getElementById(`city_errors_${type}`),
-        "phone": document.getElementById(`phone_errors_${type}`),
-        "general": document.getElementById(`general_errors_${type}`),
-        "success": document.getElementById(`general_success_${type}`)
-    };
-    Object.values(errors).forEach(error => {
-        if (error) {
-            error.innerHTML = "";
-        }
-    });
-
-    if (result.status === "OK") {
-        console.log("success");
-        const li = document.createElement("li");
-        li.innerHTML = result.description;
-        errors.success.appendChild(li);
-        if (type === "login") {
-            localStorage.setItem("userID", result.data["userID"]);
-        } else {
-            localStorage.setItem("userID", result.data["id"]);
-        }
-        localStorage.setItem("role", result.data["role"]);
-        location.href = langPrefix.value + "profile";
-    } else {
-        console.log("fail");
-        if (Array.isArray(result.message)) {
-            result.message.forEach(error => {
-                const split = error.split(" ");
-                const field = split[0];
-                split.shift();
-
-                const li = document.createElement("li");
-                li.innerHTML = split.join(" ");
-
-                if (Object.keys(errors).includes(field)) {
-                    errors[field].appendChild(li);
-                } else {
-                    errors.general.appendChild(li);
-                }
-            });
-        } else if (response.statusCode === 500 && result.message) {
-            const li = document.createElement("li");
-            if (result.message) {
-                li.innerHTML = result.message;
-            } else if (result.description) {
-                li.innerHTML = result.description;
-            } else {
-                li.innerHTML = "Unknown error";
-            }
-            errors.general.appendChild(li);
-        } else {
-            const li = document.createElement("li");
-            if (result.message) {
-                li.innerHTML = result.message;
-            } else if (result.description) {
-                li.innerHTML = result.description;
-            } else {
-                li.innerHTML = "Unknown error";
-            }
-            errors.general.appendChild(li);
-        }
-    }
-};
 
 const validateLogin = (): Boolean => {
     let errors = 0;
@@ -243,9 +172,9 @@ const login = async () => {
         localStorage.setItem("role", result.data["role"]);
         location.reload();
         // location.href = langPrefix.value + "profile";
+    } else {
+        // TODO : Add error logging to user
     }
-
-    // displayHTMLErrors(result, response, "login");
 };
 
 const validateRegister = (): Boolean => {
@@ -291,80 +220,25 @@ const register = async () => {
         localStorage.setItem("role", result.data["role"]);
         location.reload();
         // location.href = langPrefix.value + "profile";
+    } else {
+        // TODO : Add error logging to user
     }
-
-    // displayHTMLErrors(result, response, "register");
 };
 
-const logout = async () => {
-    const response = await fetch("https://api.ardeco.app/logout", {
+const logoutUser = async () => {
+    await fetch("https://api.ardeco.app/logout", {
         method: "GET",
         credentials: "include"
     });
-    await disconnect();
-    localStorage.removeItem("lang");
-    const result = await response.text();
-    console.log(result);
+    logout();
     location.href = langPrefix.value + "home";
 };
 </script>
 
 <style scoped lang="scss">
-button {
-    outline-style: solid;
-    outline-width: thin;
-}
-
 #logout {
     outline-style: solid;
     outline-width: thin;
-}
-
-.gdpr_indication {
-    text-decoration: underline;
-}
-
-.form {
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    justify-content: center;
-    border-radius: 15px;
-    padding: 25px;
-    background-color: #f4f4f4;
-    width: 30%;
-
-    div {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        padding: 3px;
-        margin: 5px 0;
-    }
-
-    .typeTitle {
-        font-size: 1.5em;
-        font-weight: bold;
-        text-align: center;
-        margin-top: -15px;
-        margin-bottom: 5px;
-    }
-
-    .gdpr_consent {
-        div {
-            display: block;
-            /*padding: 3px;
-            margin: 5px 0;*/
-
-            label {
-                margin-left: 5px;
-            }
-        }
-
-        margin-top: -3%;
-        margin-bottom: -5%;
-    }
 }
 
 #user-form {
@@ -379,35 +253,5 @@ button {
     div {
         display: block;
     }
-}
-
-#forms-wrapper {
-    display: flex;
-    flex-direction: column;
-}
-
-.mandatory_fields {
-    background-color: #e0e0e0;
-    border-radius: 15px;
-}
-
-.optional_fields {
-    background-color: #e0e0e0;
-    border-radius: 15px;
-}
-
-#login-register-wrapper {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-evenly;
-}
-
-.login-error {
-    color: red;
-}
-
-.login-success {
-    color: green;
 }
 </style>
