b5d24993d62e3011b123eb8cc0c3ce3c54d73d97
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-10-13 00:18:02 +0200

    Action menu disappear on click anywhere on the page but itself
    Fix #179

--- components/Navbar/NavbarMenu.vue
+++ components/Navbar/NavbarMenu.vue
@@ -1,51 +1,52 @@
 <template>
-    <div class="text-sm">
-        <div id="navbarMenu"
+    <div ref="navbarDiv" class="text-sm">
+        <div id="navbarMenu" ref="navbarMenu"
              class="invisible opacity-0 fixed top-32 -right-2.5 bg-white py-2.5 px-5 rounded-2xl w-52 duration-500">
             <ul>
                 <NavbarMenuLink v-if="userId" id="profileMenuOption" :alt="content.profile" :page-lang="langPrefix"
                                 image-src="images/icons/user.webp"
-                                page-name="lang-profile" @click="menuToggle">
+                                page-name="lang-profile" @click="menuToggle(false)">
                     {{ content.profile }}
                 </NavbarMenuLink>
 
                 <NavbarMenuLink v-if="userId" id="settingsMenuOption" :alt="content.settings" :page-lang="langPrefix"
                                 image-src="images/icons/settings.webp"
-                                page-name="lang-settings" @click="menuToggle">
+                                page-name="lang-settings" @click="menuToggle(false)">
                     {{ content.settings }}
                 </NavbarMenuLink>
 
                 <NavbarMenuLink v-if="userId && role === 'company'" id="companyMenuOption" :alt="content.company"
                                 :page-lang="langPrefix"
-                                image-src="images/icons/company.webp" page-name="lang-company" @click="menuToggle">
+                                image-src="images/icons/company.webp" page-name="lang-company"
+                                @click="menuToggle(false)">
                     {{ content.company }}
                 </NavbarMenuLink>
 
                 <NavbarMenuLink v-if="userId" id="ticketsMenuOption" :alt="content.tickets" :page-lang="langPrefix"
                                 image-src="images/icons/support.webp"
-                                page-name="lang-tickets" @click="menuToggle">
+                                page-name="lang-tickets" @click="menuToggle(false)">
                     {{ content.tickets }}
                 </NavbarMenuLink>
 
                 <NavbarMenuLink v-if="userId" id="feedbackMenuOption" :alt="content.feedback" :page-lang="langPrefix"
                                 image-src="images/icons/feedback.webp"
-                                page-name="lang-feedback" @click="menuToggle">
+                                page-name="lang-feedback" @click="menuToggle(false)">
                     {{ content.feedback }}
                 </NavbarMenuLink>
 
                 <NavbarMenuFunction v-if="userId" id="logoutMenuOption" :alt="content.disconnect" :fun="logoutUser"
-                                    image-src="images/icons/logout.webp" @click="menuToggle">
+                                    image-src="images/icons/logout.webp" @click="menuToggle(false)">
                     {{ content.disconnect }}
                 </NavbarMenuFunction>
 
                 <NavbarMenuLink v-if="!userId" id="loginMenuOption" :alt="content.login" :page-lang="langPrefix"
                                 :page-redirect="route.path"
-                                image-src="images/icons/logout.webp" page-name="lang-login" @click="menuToggle">
+                                image-src="images/icons/logout.webp" page-name="lang-login" @click="menuToggle(false)">
                     {{ content.login }}
                 </NavbarMenuLink>
             </ul>
         </div>
-        <div id="user" @click="menuToggle">
+        <div id="user" class="cursor-pointer" @click="menuToggle()">
             <NuxtImg id="profileImage" alt="Profile picture" class="navbar-icon rounded-[50%]" height="50px"
                      v-bind:src="imageSrc" width="50px" />
         </div>
@@ -64,13 +65,29 @@ const userId = ref<number | undefined | null>(null);
 const role = ref<string | null>("");
 const imageSrc = ref(nuxtApp.$profilePicture.url);
 
+const divRef = useTemplateRef("navbarDiv");
+const menuRef = useTemplateRef("navbarMenu");
+
 onMounted(async () => {
+    document.addEventListener("click", handleClickOutside);
+
     userId.value = await isLogged();
 
     role.value = localStorage.getItem("role");
 
     await getUSerPicture();
 });
+
+onBeforeUnmount(() => {
+    document.removeEventListener("click", handleClickOutside);
+});
+
+function handleClickOutside(event: MouseEvent) {
+    if (divRef.value) {
+        const isClickedOnMenu = divRef.value.contains(<Node>event.target);
+        if (!isClickedOnMenu) menuToggle(false);
+    }
+}
 
 async function logoutUser() {
     await fetch("https://api.ardeco.app/logout", {
@@ -101,9 +118,15 @@ async function getUSerPicture() {
     nuxtApp.$changeProfilePicture(result.data);
 }
 
-function menuToggle() {
-    const toggleMenu = document.getElementById("navbarMenu");
-    if (toggleMenu) toggleMenu.classList.toggle("invisible");
+function menuToggle(activate: boolean | null = null) {
+    if (menuRef.value) {
+        if (activate !== null) {
+            if (activate) menuRef.value.classList.remove("invisible");
+            else menuRef.value.classList.add("invisible");
+        } else {
+            menuRef.value.classList.toggle("invisible");
+        }
+    }
 }
 </script>
 
