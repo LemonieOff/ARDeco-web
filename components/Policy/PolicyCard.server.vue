450937931e46bec50b25ef2b84180bc84240685f
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-10-13 03:38:30 +0200

    New dark mode colors for product/policies pages

--- components/Policy/PolicyCard.server.vue
+++ components/Policy/PolicyCard.server.vue
@@ -1,15 +1,16 @@
 <template>
-    <div class=" w-4/5 xl:w-3/5 mx-auto rounded-xl border-2 border-[#846700] dark:border-[#D8DF81] bg-[#F5DBAF] dark:bg-[#4E360E] text-AR-Grey dark:text-[#DECDC3] text-center mb-12">
-        <h2 class="text-xl md:text-3xl text-black dark:text-AR-Beige border-b-2 border-[#846700] dark:border-[#D8DF81] underline py-1">
+    <div
+        class="w-4/5 xl:w-3/5 mx-auto rounded-xl border-2 border-[#846700] dark:border-[#4A3936] bg-[#F5DBAF] dark:bg-[#242424] text-AR-Grey dark:text-[#DECDC3] text-center mb-12">
+        <h2 class="text-xl md:text-3xl text-black dark:text-AR-Beige border-b-2 border-[#846700] dark:border-[#4A3936] underline py-1">
             {{ title }}
         </h2>
         <div class="*:p-4 md:text-xl">
-            <slot/>
+            <slot />
         </div>
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 defineProps({
     title: {
         required: true,
