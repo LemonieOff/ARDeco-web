ac55dba94b93a7d5b5ff608f42436bb2c9e1cac9
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-10-27 22:38:43 +0100

    Language changer is now in the navbar + upgraded dark mode button style
    Fix #181

--- components/Navbar/NavbarLink.server.vue
+++ components/Navbar/NavbarLink.server.vue
@@ -1,16 +1,17 @@
 <template>
-    <NuxtLink no-prefetch :id="id" :to="{ name: pageName, params: { lang: pageLang } }"
-              class="navbar-option text-AR-Beige content-center hover:text-black hover:bg-AR-Beige"
-              :class="[!mobileDisplay ? 'h-full px-4' : 'w-full py-4 last:rounded-br-2xl', mobileHidden ? 'hidden sm:inline-block' : 'inline-block']">
+    <NuxtLink
+        :class="[!mobileDisplay ? 'h-full px-4' : 'w-full py-4 last:rounded-br-2xl', mobileHidden ? 'hidden sm:inline-block' : 'inline-block']"
+        :to="{ name: pageName, params: { lang: pageLang } }"
+        class="navbar-option text-AR-Beige content-center hover:text-black hover:bg-AR-Beige"
+        no-prefetch>
         <slot></slot>
     </NuxtLink>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 defineProps({
     pageName: String,
     pageLang: String,
-    id: String,
     image: Boolean,
     mobileDisplay: Boolean,
     mobileHidden: {
