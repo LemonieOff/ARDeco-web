68e417e3cb65ad3e5b6ab3eee92fe4f2722653fd
IndiGan <72012215+IndiGan@users.noreply.github.com>
2024-10-30 22:15:11 +0100

    Blocked users now uses only one request
    Fix #223

--- pages/[[lang]]/blockedUsers.vue
+++ pages/[[lang]]/blockedUsers.vue
@@ -1,21 +1,15 @@
 <template>
     <h1 class="text-center font-bold text-xl md:text-4xl my-8">{{ content.title }}</h1>
     <div v-if="userData.length > 0 && errorMessage === ''" class="flex flex-wrap justify-center gap-5">
-        <div v-for="userData in userData" :key="userData.userId"
+        <div v-for="userData in userData" :key="userData.blocked_user_id"
              class="rounded-lg bg-AR-Floral-White dark:bg-AR-Grey"
              style="box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);">
-            <div class="flex flex-col p-5 min-w-52 max-w-52 min-h-44 max-h-44">
-                <p class="font-bold">
-                    {{ content.firstname }} <span class="font-normal">{{ userData.firstname }}</span>
-                </p>
-                <p class="font-bold">
-                    {{ content.lastname }} <span class="font-normal">{{ userData.lastname }}</span>
-                </p>
-                <p class="font-bold">
-                    {{ content.id }} <span class="font-normal">{{ userData.userId }}</span>
-                </p>
+            <div class="flex flex-col p-5 min-w-52 max-w-52">
+                <p><span class="font-bold">{{ userData.blocked_user_id }}</span> - {{ userData.blocked_user.first_name
+                    }}
+                    {{ userData.blocked_user.last_name }}</p>
                 <button class="block bg-AR-Green text-white border-none rounded text-center mt-2.5 py-2.5 px-5"
-                        @click="unblockUser(userData.userId)">
+                        @click="unblockUser(userData.blocked_user_id)">
                     {{ content.unblock }}
                 </button>
             </div>
@@ -27,12 +21,17 @@
 </template>
 
 <script lang="ts" setup>
-import { isLogged } from "public/ts/checkLogin";
+import { isLogged, userID } from "public/ts/checkLogin";
 
 interface User {
-    firstname: string;
-    lastname: string;
-    userId: number;
+    id: number;
+    user_id: number;
+    blocked_user_id: number;
+    blocked_user: {
+        id: number;
+        first_name: string,
+        last_name: string
+    };
 }
 
 const nuxtApp = useNuxtApp();
@@ -41,6 +40,8 @@ const userData = ref<User[]>([]);
 const errorMessage = ref("");
 const notificationMessage = ref("");
 const content = ref(nuxtApp.$content.blockedUsers);
+const langPrefix = nuxtApp.$langPrefix;
+const userId = ref(0);
 
 onMounted(async () => {
     await checkLogin();
@@ -48,21 +49,20 @@ onMounted(async () => {
 });
 
 async function checkLogin() {
-    const userID = await isLogged();
     if (!userID) {
-        location.href = "/login";
+        let userID_tmp = await isLogged();
+        if (!userID_tmp) {
+            location.href = `${langPrefix}login?redirect=${langPrefix}favGallery`;
+            return;
+        }
+        userId.value = userID_tmp;
+    } else {
+        userId.value = userID;
     }
 }
 
 async function getBlocked() {
-    const userID = localStorage.getItem("userID");
-    if (!userID) {
-        console.error("No user found, redirecting to login");
-        errorMessage.value = "No user found, redirecting to login";
-        return;
-    }
-
-    const response = await fetch("https://api.ardeco.app/blocked_users", {
+    const response = await fetch("https://api.ardeco.app/blocked_users?user_details", {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
@@ -77,34 +77,14 @@ async function getBlocked() {
     }
 
     const result = await response.json();
-    console.log(result);
+    console.debug(result);
 
     if (result.data.length === 0) {
         errorMessage.value = content.value.noBlockedUsers;
         return;
     }
 
-    for (let x = 0; result.data[x] !== undefined; x++) {
-        await getUserFullName(result.data[x]);
-    }
-}
-
-async function getUserFullName(userId: number) {
-    const response = await fetch(`https://api.ardeco.app/user/${userId}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: "include"
-    });
-    if (!response.ok) {
-        console.error(content.value.fetchError, response);
-        errorMessage.value = content.value.fetchError;
-        return;
-    }
-    const userDataResponse = await response.json();
-    userDataResponse.data.userId = userId;
-    userData.value.push(userDataResponse.data);
+    userData.value = result.data;
 }
 
 async function unblockUser(userId: number) {
@@ -122,7 +102,7 @@ async function unblockUser(userId: number) {
         return;
     }
 
-    userData.value = userData.value.filter(user => user.userId !== userId);
+    userData.value = userData.value.filter(user => user.blocked_user_id !== userId);
     notificationMessage.value = `User ${userId} unblocked successfully`;
     if (userData.value.length === 0) errorMessage.value = content.value.noBlockedUsers;
 }
